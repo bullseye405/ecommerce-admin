80bf902d3770c9e57d69da293453c604195784d9
Samir Prajapati <bullseye405@gmail.com>
2023-07-19 09:38:31 +0545

    completed modal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,28 @@
 'use client';
 
 import { Modal } from '@/components/ui/modal';
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
 
 const SetupPage = () => {
+  const { onOpen, isOpen } = useStoreModal();
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  
   return (
     <div className="p-4">
-
       <Modal
         isOpen
         onClose={() => {}}
         title="test"
         description="test description"
-      ></Modal>
+      >
+        Children
+      </Modal>
     </div>
   );
 };
