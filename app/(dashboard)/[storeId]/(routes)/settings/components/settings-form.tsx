a48ae34bd5febaaad26aa12e6d70ee21eccfffba
Samir <bullseye405@gmail.com>
2023-07-25 15:56:21 +0545

    completed setting page

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,147 @@
+'use client';
+
+import { Store } from '@prisma/client';
+import React, { FC, useState } from 'react';
+import * as z from 'zod';
+import { Trash } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'react-hot-toast';
+import axios, { AxiosError } from 'axios';
+import { useParams, useRouter } from 'next/navigation';
+
+import { Separator } from '@/components/ui/separator';
+import { Button } from '@/components/ui/button';
+import Heading from '@/components/ui/heading';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { AlertModal } from '@/components/modals/alert-modals';
+import { ApiAlert } from '@/components/ui/api-alert';
+import { useOrigin } from '@/hooks/use-origin';
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+const SettingsForm: FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const origin = useOrigin();
+
+  const params = useParams();
+  const router = useRouter();
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, { data });
+      router.refresh();
+      toast.success('Store updated');
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data);
+      } else {
+        toast.error('Something went wrong');
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push('/');
+      toast.success('Store deleted');
+    } catch (error) {
+      toast.error('Make sure you removed all products and categories first. ');
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button
+          variant={'destructive'}
+          size={'icon'}
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+
+      <Separator />
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="Store Name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          </div>
+
+          <Button disabled={loading} type="submit" className="ml-auto">
+            Save changes
+          </Button>
+        </form>
+      </Form>
+
+      <Separator />
+
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        description={`${origin}/api/${params.storeId}`}
+        variant="public"
+      />
+    </>
+  );
+};
+
+export default SettingsForm;
---
+++ components/ui/api-alert.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { FC } from 'react';
+import { CopyIcon, Server } from 'lucide-react';
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Badge, BadgeProps } from '@/components/ui/badge';
+import { Button } from './button';
+import { toast } from 'react-hot-toast';
+
+interface ApiAlertProps {
+  title: string;
+  description: string;
+  variant: 'public' | 'admin';
+}
+
+const textMap: Record<ApiAlertProps['variant'], string> = {
+  public: 'Public',
+  admin: 'Admin',
+};
+
+const variantMap: Record<ApiAlertProps['variant'], BadgeProps['variant']> = {
+  public: 'secondary',
+  admin: 'destructive',
+};
+
+export const ApiAlert: FC<ApiAlertProps> = ({
+  title,
+  description,
+  variant = 'public',
+}) => {
+  const onCopy = (description: string) => {
+    navigator.clipboard.writeText(description);
+    toast.success('Copied to clipboard');
+  };
+  return (
+    <Alert>
+      <Server className="h-4 w-4"></Server>
+      <AlertTitle className="flex items-center gap-x-2">
+        {title}
+        <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
+      </AlertTitle>
+      <AlertDescription className="mt-4 flex items-center justify-between">
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+          {description}
+        </code>
+        <Button
+          variant={'outline'}
+          size={'icon'}
+          onClick={() => onCopy(description)}
+        >
+          <CopyIcon />
+        </Button>
+      </AlertDescription>
+    </Alert>
+  );
+};
