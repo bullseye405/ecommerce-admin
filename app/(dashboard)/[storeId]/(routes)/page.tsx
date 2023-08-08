712e073340ad4a3f2f3cb01f15eb73b22da1d3e2
Samir <bullseye405@gmail.com>
2023-08-08 18:03:33 +0545

    wip: dash board cards

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,77 @@
-import { FunctionComponent } from 'react';
+import { CreditCard, DollarSign, Package } from 'lucide-react';
+import { FC } from 'react';
 
-interface DashboardPageProps {}
+import Heading from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { formatter } from '@/lib/utils';
+import { getTotalRevenue } from '@/actions/get-total-revenue';
+import { getSalesCount } from '@/actions/get-sales-count';
+import { getStockCount } from '@/actions/get-stock-count';
+import Overview from '@/components/overview';
 
-const DashboardPage: FunctionComponent<DashboardPageProps> = () => {
-  return <div>This is a dashboard</div>;
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: FC<DashboardPageProps> = async ({ params }) => {
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of store" />
+        <Separator />
+
+        <div className="grid gap-4 grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <DollarSign className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatter.format(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CreditCard className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">+{salesCount}</div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Products In stock
+              </CardTitle>
+              <Package className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{stockCount}</div>
+            </CardContent>
+          </Card>
+
+          <Card className="col-span-4">
+            <CardHeader>Overview</CardHeader>
+            <CardContent className="pl-2">
+              <Overview data={[]} />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
---
+++ components/overview.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+interface OverviewProps {
+  data: any;
+}
+
+const Overview: FC<OverviewProps> = ({ data }) => {
+  return <div>Overview</div>;
+};
+
+export default Overview;
